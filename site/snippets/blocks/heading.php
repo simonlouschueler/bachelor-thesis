121435974739b40aa58a5217fb17b04deebff34a
simonlouschueler <accounts@simonlou.com>
2025-12-28 14:53:43 +0100

    Correct chapter number counting.

--- site/snippets/blocks/heading.php
+++ site/snippets/blocks/heading.php
@@ -2,8 +2,8 @@
 <?php /** @var int|null $chapterNumber */ ?>
 
 <hgroup id="<?= str::slug($block->text()) ?>" class="scroll-section">
-  <?php if ($block->level() == 'h1' && isset($chapterNumber)): ?>
-	<span class="small-caps">Chapter <?= $chapterNumber ?></span>
+  <?php if ($block->level() == 'h1' && isset($chapterNumber) && $chapterNumber != 1): ?>
+	<span class="small-caps">Chapter <?= $chapterNumber - 1 ?></span>
   <?php endif ?>
   <<?= $level = $block->level()->or('h2') ?>>
 	<?= $block->text() ?>
